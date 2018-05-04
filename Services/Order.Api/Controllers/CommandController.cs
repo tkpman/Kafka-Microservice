bea470527f3c8218e80462f4b86a41a9c4681a9b
Tim Knudsen <timthesmartcoder@outlook.dk>
2018-05-04 12:12:06 +0200

    Added Validation and CommandResult.

---
+++ Services/Order.Api/Controllers/CommandController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Order.Api.Application.Models;
+
+namespace Order.Api.Controllers
+{
+    [Route("api/{controller}")]
+    public class CommandController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CommandController(IMediator mediator)
+        {
+            if (mediator == null)
+                throw new ArgumentNullException(nameof(mediator));
+
+            this._mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewOrder newOrder)
+        {
+            return new CreatedResult("", "");
+        }
+    }
+}
