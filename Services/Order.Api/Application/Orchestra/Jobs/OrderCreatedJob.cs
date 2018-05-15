cec9a013dec650fe5958d21237ffa1d5f45d6423
Tim Knudsen <timthesmartcoder@outlook.dk>
2018-05-15 09:10:46 +0200

    Cleanup orchestra in order service.

---
+++ Services/Order.Api/Application/Orchestra/Jobs/OrderCreatedJob.cs
@@ -0,0 +1,84 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using HostedServices;
+using Microsoft.Extensions.Configuration;
+using Order.Api.Application.Events;
+using Order.Api.Application.Orchestra.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitOfWorks.Abstractions;
+using UnitOfWorks.EntityFrameworkCore;
+
+namespace Order.Api.Application.Orchestra.Jobs
+{
+    public class OrderCreatedJob
+        : IJob
+    {
+        private readonly IConfiguration _configuration;
+
+        public OrderCreatedJob(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public void Run(CancellationToken cancellationToken)
+        {
+            var consumerConfig = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", this._configuration["Kafka:BootstrapServers"] },
+                { "group.id", Guid.NewGuid() },
+                { "schema.registry.url", this._configuration["Kafka:SchemaRegistryUrl"] }
+            };
+
+            using (var consumer = new Consumer<string, OrderCreated>(consumerConfig, new AvroDeserializer<string>(), new AvroDeserializer<OrderCreated>()))
+            {
+                IUnitOfWork unitOfWork = new UnitOfWork<Infrastructure.OrderOrchestraDbContext>(new OrderOrchestraDbContext());
+                IRepository<Entities.Order> orderRepository = unitOfWork.GetRepository<Entities.Order>();
+
+                consumer.OnMessage += (o, e) =>
+                {
+                    var order = new Entities.Order()
+                    {
+                        OrderId = e.Value.id,
+                        CustomerId = e.Value.customerId,
+                        Date = DateTime.Parse(e.Value.date),
+                        Products = e.Value.products.Select(x => new Entities.OrderProduct()
+                        {
+                            ProductId = x.id,
+                            Quantity = x.Quantity
+                        }).ToList()
+                    };
+
+                    orderRepository.Add(order);
+
+                    var result = unitOfWork.SaveChanges().Result;
+
+                    if (!result.IsSuccessfull())
+                        throw new Exception();
+                };
+
+                consumer.OnError += (_, e) =>
+                {
+                    Console.WriteLine("Error: " + e.Reason);
+                };
+
+                consumer.OnConsumeError += (_, e) =>
+                {
+                    Console.WriteLine("Consume error: " + e.Error.Reason);
+                };
+
+                consumer.Subscribe("order-created");
+
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    consumer.Poll(100);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Services/Shared/HostedServices/JobHostingService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedServices
+{
+    public class JobHostingService
+        : IHostedService
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly List<Task> _tasks;
+        private readonly List<IJob> _jobs;
+        private readonly IServiceProvider _serviceProvider;
+
+        public JobHostingService(IServiceProvider serviceProvider)
+        {
+            this._cancellationTokenSource = new CancellationTokenSource();
+            this._tasks = new List<Task>();
+            this._jobs = new List<IJob>();
+            this._serviceProvider = serviceProvider;
+        }
+
+        public void AddJob(IJob job)
+        {
+            if (job == null)
+                throw new ArgumentNullException(nameof(job));
+
+            this._jobs.Add(job);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            foreach(var job in this._jobs)
+                Task.Factory.StartNew(
+                    () => job.Run(this._cancellationTokenSource.Token),
+                    TaskCreationOptions.LongRunning);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this._cancellationTokenSource.Cancel();
+
+            var tasks = Task.WhenAll(this._tasks);
+
+            return Task.WhenAny(tasks, Task.Delay(Timeout.Infinite, cancellationToken));
+        }
+    }
+
+    public interface IJob
+    {
+        void Run(CancellationToken cancellationToken);
+    }
+
+    public abstract class Job
+        : IJob
+    {
+        public abstract void Run(CancellationToken cancellationToken);
+    }
+}
